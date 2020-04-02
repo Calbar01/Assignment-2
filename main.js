31d2fd528c6cebed3aa1b13a47a49c02a109d90d
Calbar01 <56161940+Calbar01@users.noreply.github.com>
2020-04-02 14:19:37 +0100

    modal

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 let modalBtn = document.getElementById("modal-btn")
-
-let modal = document.querySelector(".modal-container")
-
+let modal = document.querySelector(".modal")
+let closeBtn = document.querySelector(".close-btn")
 modalBtn.onclick = function(){
-    modal.style.display = "block"
+  modal.style.display = "block"
 }
-
+closeBtn.onclick = function(){
+  modal.style.display = "none"
+}
 window.onclick = function(e){
-    if(e.target == modal){
-        modal.style.display = "none"
-    }
+  if(e.target == modal){
+    modal.style.display = "none"
+  }
 }
