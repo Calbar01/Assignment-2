a9465c62fe569c71b67d51ba807e5bf7dc3eb347
Calbar01 <56161940+Calbar01@users.noreply.github.com>
2020-04-03 12:23:16 +0100

    validation

--- main.js
+++ main.js
@@ -1,18 +1,5 @@
-//Help found from https://www.youtube.com/watch?v=gLWIYk0Sd387
-
-/*Creates function to 'grab' the modal-button, add an event listener, and if the user clicks on the button, 
-the modal will change to 'flex' to be able to view the modal*/
-document.getElementById('modal-button').addEventListener('click',
-function(){
-    document.querySelector('.bg-modal').style.display = 'flex';
-});
-
-/*Does opposite of previous function, resets the display to 'none' if the user clicks on the exit button*/
-document.querySelector('.exit').addEventListener('click',
-function(){
-    document.querySelector('.bg-modal').style.display = 'none';
-});
-
+/*This function takes the input from the form, and makes sure the user has entered a value in.
+Help found from https://www.geeksforgeeks.org/form-validation-using-html-javascript/*/
 function validation(){
     let firstname = document.forms["messageForm"]["user_firstname"];
     let surname = document.forms["messageForm"]["user_surname"];
@@ -45,3 +32,26 @@ function validation(){
 
     return true;
 }
+
+
+
+
+//Help found from https://www.youtube.com/watch?v=gLWIYk0Sd387
+
+/*Creates function to 'grab' the modal-button, add an event listener, and if the user clicks on the button, 
+the modal will change to 'flex' to be able to view the modal*/
+if (validation == true){
+    document.getElementById('modal-button').addEventListener('click',
+    function(){
+        document.querySelector('.bg-modal').style.display = 'flex';
+    });
+}
+
+
+/*Does opposite of previous function, resets the display to 'none' if the user clicks on the exit button*/
+document.querySelector('.exit').addEventListener('click',
+function(){
+    document.querySelector('.bg-modal').style.display = 'none';
+    
+});
+
